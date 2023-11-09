b24ae1321d56e7a312953315f302827079866599
barbara <106076374+barbaramoorais@users.noreply.github.com>
2023-11-09 08:21:05 -0300

    salva

--- main.js
+++ main.js
@@ -8,10 +8,9 @@ while(contador<listaDeTeclas.length){
     const efeito = listaDeTecla[contador].classList[1];
     const idAudio = "#som_"+efeito;
     listaDeTeclas[contador].onclick = function(){
-        tocaSom("#som_tecla_aplausos");
         tocaSom(idAudio);
     }
     contador = contador + 1;
     console.log(contador);
-    
+
 }
